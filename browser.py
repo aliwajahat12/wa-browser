5fd70973a3de040a218eeaa3137a9d7861b744d6
Wajahat Ali <aliwajahat1214@gmail.com>
2025-03-19 04:32:10 +0500

    Completed chapter 1 Exercises done till 1-4

---
+++ browser.py
@@ -0,0 +1,114 @@
+import os
+import socket
+import ssl
+import urllib.parse
+
+
+class URL:
+    def __init__(self, url):
+        if url.startswith('data'):
+            self.scheme = 'data'
+            self.data_url = url
+            return
+        self.scheme, url = url.split("://", 1)
+        assert self.scheme in ["http", "https", "file"]
+        if self.scheme == "https":
+            self.port = 443
+        else:
+            self.port = 80
+        if "/" not in url:
+            url += "/"
+        self.host, url = url.split("/", 1)
+        if ':' in self.host:
+            self.host, port = self.host.split(':', 1)
+            self.port = int(port)
+        if not self.scheme == 'file':
+            url = "/" + url
+        self.path = url
+
+    def request(self):
+        if self.scheme == "file":
+            if not os.path.isfile(self.path):
+                raise FileNotFoundError(f"File not found: {self.path}")
+            with open(self.path, "r", encoding='utf-8') as f:
+                return f.read()
+        elif self.scheme == 'data':
+            prefix = 'data:'
+            data_content = self.data_url[len(prefix):]
+            if ',' not in data_content:
+                raise ValueError("Invalid data URL format")
+            metadata, data = data_content.split(',', 1)
+            decoded_data = urllib.parse.unquote(data)
+            return decoded_data
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=socket.IPPROTO_TCP)
+        s.connect((self.host, self.port))
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+        request = f'GET {self.path} HTTP/1.1\r\n'
+        headers = {
+            'Host': self.host,
+            'Connection': 'close',
+            'User-Agent': 'CustomSimpleClient/1.0'
+        }
+        for header, value in headers.items():
+            request += f'{header}: {value}\r\n'
+        request += '\r\n'
+        s.send(request.encode("utf-8"))
+        response = s.makefile("r", encoding="utf-8", newline="\r\n")
+        status_line = response.readline()
+        version, status, explanation = status_line.split(" ", 2)
+        response_headers = {}
+        while True:
+            line = response.readline()
+            if line == '\r\n':
+                break
+            header, value = line.split(":", 1)
+            response_headers[header.casefold()] = value
+        assert "transfer-encoding" not in response_headers
+        assert "content-encoding" not in response_headers
+
+        content = response.read()
+        s.close()
+
+        return content
+
+
+def show(body):
+    in_tag = False
+    i = 0
+    while i < len(body):
+        c = body[i]
+        if c == '<':
+            in_tag = True
+            i += 1
+        elif c == '>':
+            in_tag = False
+            i += 1
+        elif not in_tag:
+            if body.startswith('&lt', i):
+                print("<", end="")
+                i += 4
+            elif body.startswith('&gt', i):
+                print(">", end="")
+                i += 4
+            else:
+                print(c, end="")
+                i += 1
+        else:
+            i += 1
+
+
+def load(url: URL):
+    body = url.request()
+    show(body)
+
+
+if __name__ == "__main__":
+    import sys
+
+    default_file = "/temp/test.html"
+    if len(sys.argv) > 1:
+        load(URL(sys.argv[1]))
+    else:
+        load(URL(f'file://{default_file}'))
---
+++ readme.md
@@ -0,0 +1,1 @@
+Implementation of the book https://browser.engineering/
